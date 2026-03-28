4c359809966307c57936bfd93b3b85bb74052797
juokho <juok4069@gmail.com>
2026-03-28 23:38:02 +0900

    통합2
    통합2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend',
   server: {
     proxy: {
       '/api': {
@@ -13,4 +12,7 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist'
+  }
 })
