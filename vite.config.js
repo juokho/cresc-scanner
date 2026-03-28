af66f74821ea71c01ea0a94f65ebb50054ff8e84
juokho <juok4069@gmail.com>
2026-03-28 23:51:31 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'frontend',
   server: {
     proxy: {
       '/api': {
@@ -13,6 +14,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist'
+    outDir: '../dist'
   }
 })
